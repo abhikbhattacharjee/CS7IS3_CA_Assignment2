cca849920188a37c0accb4d9cb1e3ba8f9dbb8e6
Abhik Bhattacharjee <abhikbhattacharjee.mit@gmail.com>
2022-11-19 16:39:26 +0000

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import org.apache.lucene.queryparser.classic.ParseException;
+
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         fbis_parser.fbis_parser_main(null);
-        indexer.indexer(null);
+        ft_parser.ft_parser_main(null);
+        latimes_parser.latimes_parser_main(null);
+        fr94_parser.fr94_parser_main(null);
+        combined_indexing.combined_indexer(null);
+        queries.queries(null);
     }
 }
